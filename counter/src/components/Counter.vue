30cea4bf81bcb86b353b7a25bbe229e55a6a5fd6
luna <1661639144@qq.com>
2019-09-02 16:17:04 +0800

    配置环境

---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+
+    <h2>Counter</h2>
+    <p>
+    <button @click="dec">-</button>
+     <span>count{{count}}</span>
+     <button @click="inc">+</button>
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+  //数据初始化
+  data(){
+        return {
+          count:0
+        }
+  },  
+ 
+  //事件处理函数
+  methods:{
+   
+    //增加
+    inc(){
+      const count=this.count
+      this.count=count+1
+    },
+
+    //减法
+     dec(){
+      const count=this.count
+      this.count=count-1
+    },
+    
+  }
+}
+</script>
+
+<style scoped>
+
+button{
+  margin:0 20px; 
+}
+</style>
---
+++ counter/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">    
+    <ul>
+    <li v-for="n in 30" :key="n">
+     {{n}}<Counter></Counter>
+    </li>
+  </ul>  
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue' 
+export default {
+  name: 'HelloWorld',
+  components:{
+    Counter
+ }
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
